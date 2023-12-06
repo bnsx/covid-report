053ba10e1cf5800cab58591fc434c2d8b6179201
Ammarin Boonsong <ammarin.bns@gmail.com>
2023-12-06 23:26:37 +0700

    make responsive navbar for mobile version.

--- components/Navbar/index.tsx
+++ components/Navbar/index.tsx
@@ -1,8 +1,13 @@
 "use client";
 import { GitHubLogoIcon } from "@radix-ui/react-icons";
+import { MoreHorizontal } from "lucide-react";
 import Link from "next/link";
+import { Button } from "../ui/button";
+import { useState } from "react";
+import MobileVersion from "./mobile";
 
-const data = [
+export type navLink = { label: string; url: string; icon?: JSX.Element };
+const data: navLink[] = [
   { label: "หน้าแรก", url: "/" },
   { label: "อาการ", url: "/symptom" },
   {
@@ -11,9 +16,26 @@ const data = [
     icon: <GitHubLogoIcon className="w-6 h-6" />,
   },
 ];
+
 export default function Navbar() {
+  const [open, setOpen] = useState(false);
+  const onOpenChange = () => setOpen(!open);
+
+  const renderNavLink = (item: navLink) => {
+    return (
+      <Link
+        key={item.url}
+        href={item.url}
+        target={item.url.startsWith("http") ? "_blank" : ""}
+        className="text-lg hover:text-muted-foreground text-white duration-300"
+      >
+        {item.icon ? item.icon : item.label}
+      </Link>
+    );
+  };
+
   return (
-    <div className="xl:px-10 p-5 bg-black xl:flex justify-between">
+    <div className="xl:px-10 p-5 bg-black flex items-center justify-between">
       <Link
         href={"/"}
         className="text-2xl hover:text-muted-foreground text-white duration-300"
@@ -21,17 +43,17 @@ export default function Navbar() {
         Hypersonix
       </Link>
       <div className="xl:flex items-center gap-6 hidden">
-        {data.map((v) => (
-          <Link
-            key={v.url}
-            href={v.url}
-            target="_blank"
-            className="text-lg hover:text-muted-foreground text-white duration-300"
-          >
-            {v.icon ? v.icon : v.label}
-          </Link>
-        ))}
+        {data.map((item) => renderNavLink(item))}
       </div>
+      <Button
+        type="button"
+        variant={"link"}
+        className="xl:hidden"
+        onClick={onOpenChange}
+      >
+        <MoreHorizontal className="text-white" />
+      </Button>
+      {open ? <MobileVersion onOpenChange={onOpenChange} data={data} /> : null}
     </div>
   );
 }
---
+++ components/Navbar/mobile.tsx
@@ -0,0 +1,48 @@
+import { X } from "lucide-react";
+import { Button } from "../ui/button";
+import { navLink } from ".";
+import Link from "next/link";
+
+interface Props {
+  onOpenChange: () => void;
+  data: navLink[];
+}
+export default function MobileVersion({ onOpenChange, data }: Props) {
+  return (
+    <div className="fixed top-0 left-0 w-full h-full bg-black text-white z-50 flex justify-center items-center">
+      <div className="relative w-full">
+        <div className="absolute top-5 right-5">
+          <Button
+            variant={"link"}
+            onClick={onOpenChange}
+            className="text-white text-xl"
+          >
+            <X />
+          </Button>
+        </div>
+        <div className="w-full h-screen flex justify-center items-center">
+          <div className="flex flex-col w-full gap-1">
+            {data.map((v) => {
+              if (v.icon) {
+                return (
+                  <Button className="w-full" variant={"outline"}>
+                    <Link key={v.url} href={v.url}>
+                      {v.icon}
+                    </Link>
+                  </Button>
+                );
+              }
+              return (
+                <Button className="w-full" variant={"outline"}>
+                  <Link key={v.url} href={v.url}>
+                    {v.label}
+                  </Link>
+                </Button>
+              );
+            })}
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
